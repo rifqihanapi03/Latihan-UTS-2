9e84d04bb2e5f36f02fd89f6409446b060432a3e
Qhi <Qhi@DESKTOP-EC2VDIV>
2017-11-14 23:30:42 +0700

    latihan UTS 2 - PBO-11

---
+++ src/Model/GajiPegawai.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author LENOVO 
+ * PBO11_10116463_Mochamad Fajar Ismatulloh
+ */
+public class GajiPegawai {
+
+    private String nama;
+    private String alamat;
+    private int uangTransport;
+    private int uangTunjangan;
+    private int gajiPokok;
+    private int totalGaji;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public int getUangTransport() {
+        return uangTransport;
+    }
+
+    public void setUangTransport(int uangTransport) {
+        this.uangTransport = uangTransport;
+    }
+
+    public int getUangTunjangan() {
+        return uangTunjangan;
+    }
+
+    public void setUangTunjangan(int uangTunjangan) {
+        this.uangTunjangan = uangTunjangan;
+    }
+
+    public int getGajiPokok() {
+        return gajiPokok;
+    }
+
+    public void setGajiPokok(int gajiPokok) {
+        this.gajiPokok = gajiPokok;
+    }
+
+    public int getTotalGaji() {
+        return totalGaji;
+    }
+
+    public int totalGaji(int uangTunjangan, int uangTransport, int gajiPokok) {
+        totalGaji = uangTunjangan + uangTransport + gajiPokok;
+        return totalGaji;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
